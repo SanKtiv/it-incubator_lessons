05d87bc46de624fcaf791a9cf0cdee9d9eeaaa76
SanKtiv <aktitorov@gmail.com>
2023-10-13 08:34:59 +0300

    update for working

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express, {Request, Response} from 'express'
+import {productsRouter} from './routers/products-router';
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
+
+const  parserMiddleware = express.json()
+app.use(parserMiddleware)
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+    res.send('HI SAMURAI')
 })
+
+app.use('/products', productsRouter)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
