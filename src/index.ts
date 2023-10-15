a747296f12f9f564abdc5a516bbd519584dbb2c4
SanKtiv <aktitorov@gmail.com>
2023-10-16 00:11:36 +0300

    add code with get, post, delete for videos

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, {Request, Response} from 'express'
-import {productsRouter} from './routers/products-router';
+import {productsRouter, videosRouter} from './routers/products-router';
+
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -10,7 +11,9 @@ app.get('/', (req: Request, res: Response) => {
     res.send('HI SAMURAI')
 })
 
+
 app.use('/products', productsRouter)
+app.use('/', videosRouter)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
