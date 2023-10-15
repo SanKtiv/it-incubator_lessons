d2d2d79bbd12ae1c3eea995cab50e59157be09b6
SanKtiv <aktitorov@gmail.com>
2023-10-16 00:12:14 +0300

    update code for video

--- src/repositories/products-repository.ts
+++ src/repositories/products-repository.ts
@@ -21,4 +21,83 @@ export  const productsRepository = {
         products.unshift(newProduct)
         return newProduct
     },
+    deleteProduct(title: string): boolean | null {
+        if (!title.trim()) {
+            return null
+        }
+
+        let indexDeletedProduct: number = -1
+
+        for (let i = 0; i < products.length; i++) {
+            if (products[i].title === title) {
+                indexDeletedProduct = i
+            }
+        }
+
+        if (indexDeletedProduct >= 0) {
+            products.splice(indexDeletedProduct, 1)
+            return true
+        } else {
+            return null
+        }
+    }
+}
+
+type VideoType = {
+    id: number,
+    title: string,
+    author: string,
+    canBeDownloaded: boolean,
+    minAgeRestriction: null,
+    createdAt: string,
+    publicationDate: string,
+    availableResolutions: Array<string>
+}
+
+let dateNow = new Date
+
+const defaultVideo: VideoType = {
+    id: 0,
+    title: 'Zero',
+    author: 'Zerovich',
+    canBeDownloaded: true,
+    minAgeRestriction: null,
+    createdAt: dateNow.toISOString(),
+    publicationDate: dateNow.toISOString(),
+    availableResolutions: ['P1080']
+}
+
+const videos: VideoType[] = [defaultVideo]
+
+export const videosRepository = {
+    getVideo(): VideoType[] {
+        return videos
+    },
+    createVideo(title: string, author: string, availableResolutions: Array<string>): VideoType | null {
+
+        const resolutionsTrue: Array<string> = [ 'P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160' ]
+
+        if (!title.trim() || !author.trim()) {
+            return null
+        }
+
+        if (resolutionsTrue.indexOf(availableResolutions[0]) == -1) {
+            return  null
+        }
+
+        const newVideo: VideoType = {
+            id: Date.now(),
+            title,
+            author,
+            canBeDownloaded: true,
+            minAgeRestriction: null,
+            createdAt: dateNow.toISOString(),
+            publicationDate: dateNow.toISOString(),
+            availableResolutions
+        }
+
+        videos.push(newVideo)
+
+        return newVideo
+}
 }
