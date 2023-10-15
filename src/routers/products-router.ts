d8b02b8f3e637044e8e80142329d7e215c23f837
SanKtiv <aktitorov@gmail.com>
2023-10-16 00:12:19 +0300

    update code for video

--- src/routers/products-router.ts
+++ src/routers/products-router.ts
@@ -1,7 +1,9 @@
 import {Request, Response, Router} from 'express';
 import {productsRepository} from '../repositories/products-repository';
+import {videosRepository} from "../repositories/products-repository";
 
 export const productsRouter = Router({})
+export const videosRouter = Router ({})
 productsRouter.get('/', (req: Request, res: Response) => {
     const products = productsRepository.getProducts()
     res.send(products)
@@ -14,4 +16,31 @@ productsRouter.post('/', (req: Request, res: Response) => {
     } else {
         res.sendStatus(404)
     }
+})
+productsRouter.delete('/', (req: Request, res: Response) => {
+    const deletedProduct = productsRepository.deleteProduct(req.body.title)
+    if (deletedProduct) {
+        res.status(201).send('delete')
+    } else {
+        res.sendStatus(404)
+    }
+})
+
+productsRouter.delete('/testing/all-data', (req: Request, res: Response) => {
+    res.sendStatus(204)
+})
+
+productsRouter.get('/api/videos', (req: Request, res: Response) => {
+    const products = productsRepository.getProducts()
+    res.send(products)
+})
+
+videosRouter.get('/videos', (req: Request, res: Response) => {
+    const videos = videosRepository.getVideo()
+    res.send(videos)
+})
+
+videosRouter.post('/videos', (req: Request, res: Response) => {
+    const videos = videosRepository.createVideo(req.body.title, req.body.author, req.body.availableResolutions)
+    res.status(201).send(videos)
 })
