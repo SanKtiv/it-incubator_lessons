3c1f33ee60b41c50f90578797c68d801398a41d7
SanKtiv <aktitorov@gmail.com>
2023-10-16 15:58:02 +0300

    add method put, and get by id

--- src/repositories/products-repository.ts
+++ src/repositories/products-repository.ts
@@ -48,8 +48,21 @@ type VideoType = {
     title: string,
     author: string,
     canBeDownloaded: boolean,
-    minAgeRestriction: null,
+    minAgeRestriction: null | number,
     createdAt: string,
+    publicationDate: string,
+    availableResolutions: Array<string>
+}
+
+type CreateErrorType = {
+    errorsMessages: [{message: string, field: string}]
+}
+
+type DataForUpdateVideo = {
+    title: string,
+    author: string,
+    canBeDownloaded: boolean,
+    minAgeRestriction: null | number,
     publicationDate: string,
     availableResolutions: Array<string>
 }
@@ -67,22 +80,43 @@ const defaultVideo: VideoType = {
     availableResolutions: ['P1080']
 }
 
-const videos: VideoType[] = [defaultVideo]
+const resolutionsTrue: Array<string> = [ 'P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160' ]
+
+const errorMes: CreateErrorType = {
+    errorsMessages: [{message: 'string', field: 'string'}]
+}
+
+const videos: VideoType[] = [defaultVideo] // Database of Videos
 
 export const videosRepository = {
     getVideo(): VideoType[] {
         return videos
     },
-    createVideo(title: string, author: string, availableResolutions: Array<string>): VideoType | null {
+    getVideoId(videoId: string): VideoType | undefined {
+        if (!videoId.trim) {
+            return undefined
+        }
+        const video: VideoType | undefined = videos.find(v => v.id === Number(videoId))
+        return video
+    },
+    createVideo(title: string, author: string, availableResolutions: Array<string>): VideoType | CreateErrorType {
 
-        const resolutionsTrue: Array<string> = [ 'P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160' ]
+        if (!title.trim()) {
+            errorMes.errorsMessages[0].message = 'Wrong title'
+            errorMes.errorsMessages[0].field = 'title is empty'
+            return errorMes
+        }
 
-        if (!title.trim() || !author.trim()) {
-            return null
+        if (!author.trim()) {
+            errorMes.errorsMessages[0].message = 'Wrong author'
+            errorMes.errorsMessages[0].field = 'author is empty'
+            return errorMes
         }
 
         if (resolutionsTrue.indexOf(availableResolutions[0]) == -1) {
-            return  null
+            errorMes.errorsMessages[0].message = 'Wrong Resolutions'
+            errorMes.errorsMessages[0].field = 'Resolutions is incorrect'
+            return errorMes
         }
 
         const newVideo: VideoType = {
@@ -99,5 +133,55 @@ export const videosRepository = {
         videos.push(newVideo)
 
         return newVideo
-}
+    },
+    updateVideo(videoId: string, dataUpdate: any): CreateErrorType | number {
+
+        if (!dataUpdate.title.trim()) {
+            errorMes.errorsMessages[0].message = 'Wrong title'
+            errorMes.errorsMessages[0].field = 'title is empty'
+            return errorMes
+        }
+
+        if (!dataUpdate.author.trim()) {
+            errorMes.errorsMessages[0].message = 'Wrong author'
+            errorMes.errorsMessages[0].field = 'author is empty'
+            return errorMes
+        }
+
+        if (resolutionsTrue.indexOf(dataUpdate.availableResolutions[0]) == -1) {
+            errorMes.errorsMessages[0].message = 'Wrong Resolutions'
+            errorMes.errorsMessages[0].field = 'Resolutions is incorrect'
+            return errorMes
+        }
+
+        if (!(dataUpdate.canBeDownloaded === 'true' || dataUpdate.canBeDownloaded === 'false')) {
+            errorMes.errorsMessages[0].message = 'Wrong canBeDownloaded'
+            errorMes.errorsMessages[0].field = 'canBeDownloaded is incorrect'
+            return errorMes
+        }
+
+        if (!(dataUpdate.minAgeRestriction >= 1 && dataUpdate.minAgeRestriction <= 18 || dataUpdate.minAgeRestriction === null)) {
+            errorMes.errorsMessages[0].message = 'Wrong minAgeRestriction'
+            errorMes.errorsMessages[0].field = 'minAgeRestriction is incorrect'
+            return errorMes
+        }
+
+        const findedVideo = videos.find(v => v.id === Number(videoId))
+
+        if (findedVideo) {
+            videos.forEach(v => {
+                if (v.id === Number(videoId)) {
+                    v.title = dataUpdate.title
+                    v.author = dataUpdate.author
+                    // v.availableResolutions = dataUpdate.availableResolutions
+                    // v.canBeDownloaded = dataUpdate.canBeDownloaded
+                    // v.minAgeRestriction = dataUpdate.minAgeRestriction
+                    // v.publicationDate = dataUpdate.publicationDate
+                }
+            })
+            return 204
+        } else {
+            return 404
+        }
+    }
 }
